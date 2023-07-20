ebe99580e38cdd1da9a3c43d57e329e4f77ef6ff
Eric Ubaga <ubaga91@gmail.com>
2023-07-20 18:47:14 +0300

    added file 1-insertion_sort_list.c that implements insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,7 +7,66 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t prev = NULL;
-	listint_t next = NULL;
-	size_t
+	listint_t *current = NULL;
+	listint_t *next = NULL;
+	listint_t *head = NULL;
+
+	if (!list && !(*list))
+		return;
+	head = *list;
+	current = head;
+	while (current->next)
+	{
+		next = current->next;
+		if (current->n > next->n)
+			reverse_swap(list, next);
+		else
+			current = current->next;
+	}
+}
+
+/**
+ * reverse_swap - swap pointers towards the first node
+ * @list: pointer to pointer to first node
+ * @head: the pointer to start swapping towards the first node
+*/
+void reverse_swap(listint_t **list, listint_t *head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = NULL;
+
+	if (!head)
+		return;
+	while (head->prev)
+	{
+		current = head;
+		prev = head->prev;
+		if (prev->n > current->n)
+		{
+			swap_list(prev, current);
+			head = current;
+			if (head->prev == NULL)
+				*list = head;
+			print_list(*list);
+		}
+		else
+			return;
+	}
+}
+
+/**
+ * swap_list - swaps two doubly linked list nodes
+ * @current: first node
+ * @next: second node
+*/
+void swap_list(listint_t *current, listint_t *next)
+{
+	current->next = next->next;
+	if (next->next)
+		next->next->prev = current;
+	next->next = current;
+	next->prev = current->prev;
+	if (current->prev)
+		current->prev->next = next;
+	current->prev = next;
 }
--- sort.h
+++ sort.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -14,16 +13,16 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
-
-
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void swap_list(listint_t *current, listint_t *next);
+void reverse_swap(listint_t **list, listint_t *head);
 
 #endif
