1c2b6614949309320162ab0774744fac3d67547f
Richard Muchira <rwachira.rw@gmail.com>
2023-07-24 14:55:07 +0300

    modified the whole code

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,122 +1,67 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * swap_list - swaps two doubly linked list nodes
- * @current: first node
- * @next: second node
+ * swap - swaps a node with the next node in the list
+ * @list: pointer to a pointer to the start of the list
+ * @node: node to swap
  */
-void swap_list(listint_t *current, listint_t *next)
+void swap(listint_t **list, listint_t *node)
 {
-	current->next = next->next;
-	if (next->next != NULL)
-		next->next->prev = current;
-	next->next = current;
-	next->prev = current->prev;
-	if (current->prev != NULL)
-		current->prev->next = next;
-	current->prev = next;
-}
-
-/**
- * sort_forward - bubble sort the list in forward direction
- * @start: the node to start at
- * @stop: the node to stop sorting
- * @list: address of the pointer to first node
- */
-void sort_forward(listint_t *start, listint_t *stop, listint_t **list)
-{
-	listint_t *iter = NULL;
-	listint_t *next = NULL;
-	int swapped = 0;
-
-	if (!list || !start || start == stop)
+	if (!node|| node->next == NULL || !list|| !(*list))
 		return;
-
-	iter = start;
-	while (iter->next && iter != stop)
-	{
-		next = iter->next;
-		if (iter->n > next->n)
-		{
-			swap_list(iter, next);
-			swapped++;
-			if (iter->prev == NULL)
-				*list = iter;
-			print_list(*list);
-		}
-		else
-			iter = iter->next;
-	}
-
-	if (swapped)
-		sort_reversed(iter, start, list);
+	node->next->prev = node->prev;
+	if (node->prev)
+		node->prev->next = node->next;
+	else
+		*list = node->next;
+	node->prev = node->next;
+	node->next = node->next->next;
+	node->prev->next = node;
+	if (node->next)
+		node->next->prev = node;
 }
 
 /**
- * sort_reversed - bubble sort the list in reversed direction
- * @start: the node to start at
- * @stop: the node to stop sorting
- * @list: address of the pointer to first node
- */
-void sort_reversed(listint_t *start, listint_t *stop, listint_t **list)
-{
-	listint_t *iter = NULL;
-	listint_t *prev = NULL;
-	int swapped = 0;
-
-	if (!list || !start || start == stop)
-		return;
-
-	iter = start;
-	while (iter->prev && iter != stop)
-	{
-		prev = iter->prev;
-		if (iter->n < prev->n)
-		{
-			swap_list(prev, iter);
-			swapped++;
-			if (iter->prev == NULL)
-				*list = iter;
-			print_list(*list);
-		}
-		else
-			iter = iter->prev;
-	}
-
-	if (swapped)
-		sort_forward(iter, start, list);
-}
-
-/**
- * cocktail_sort_list - sorts a list using the Cocktail Shaker sort
- * @list: pointer to pointer to the first node
+ * cocktail_sort_list - sorts a doubly linked list of integers in ascending
+ * order using the Cocktail shaker sort algorithm
+ * @list: Pointer to a pointer to the head of the list
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *iter = NULL;
-	listint_t *next = NULL;
-	listint_t *stop = NULL;
-	int swapped = 0;
+	int swapped = 1;
+	listint_t *temp;
 
-	if (!list || !(*list) || !((*list)->next))
+	if (list == NULL || *list == NULL)
 		return;
-
-	iter = *list;
-	while (iter->next && iter != stop)
+	temp = *list;
+	while (swapped)
 	{
-		next = iter->next;
-		if (iter->n > next->n)
+		swapped = 0;
+		while (temp->next != NULL)
 		{
-			swap_list(iter, next);
-			swapped++;
-			if (iter->prev == NULL)
-				*list = iter;
-			print_list(*list);
+			if (temp->next->n < temp->n)
+			{
+				swap(list, temp);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				temp = temp->next;
 		}
-		else
-			iter = iter->next;
+		if (swapped == 0)
+			break;
+		swapped = 0;
+		while (temp->prev != NULL)
+		{
+			if (temp->prev->n > temp->n)
+			{
+				swap(list, temp->prev);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				temp = temp->prev;
+		}
 	}
-
-	if (swapped)
-		sort_reversed(iter, *list, list);
 }
--- sort.h
+++ sort.h
@@ -28,11 +28,9 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quickSort(int *array, size_t low, size_t high, size_t size);
 size_t partition(int *array, size_t low, size_t high, size_t size);
-void swap(int *a, int *b);
+void swap(listint_t **list, listint_t *node);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-void sort_reversed(listint_t *start, listint_t *stop, listint_t **list);
-void sort_forward(listint_t *start, listint_t *stop, listint_t **list);
 void counting_sort(int *array, size_t size);
 
 #endif
